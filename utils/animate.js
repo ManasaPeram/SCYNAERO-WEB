60b4cedf6f0984c47b7ebaf255e9e2617b303b78
manasaperam <manasaperam1980@gmail.com>
2024-05-30 16:19:50 +0530

    added imports

--- utils/animate.js
+++ utils/animate.js
@@ -29,6 +29,21 @@ export const tagVariants = {
     }
 }
 
+export const titleVariants = {
+    "offscreen":{
+        opacity:0,
+        y:10
+    },
+    "onscreen":{
+        opacity:1,
+        y:0,
+        transition:{
+            type:"spring",
+            duration:2.2
+        }
+    }
+}
+
 export const desVariants = {
     "offscreen":{
         opacity:0,
