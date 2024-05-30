f9ef4b9d9fd0f0acd26fba3a534f8e4e005c72ef
manasaperam <manasaperam1980@gmail.com>
2024-05-30 16:01:22 +0530

    deleted old file1

--- next.config.mjs
+++ next.config.mjs
@@ -5,8 +5,6 @@ const nextConfig = {
         domains: ['arthurgrand.com', 'i.pinimg.com', 'media.licdn.com','static.vecteezy.com','img.freepik.com','st2.depositphotos.com','watermark.lovepik.com','agcdn-1d97e.kxcdn.com','t3.ftcdn.net','encrypted-tbn0.gstatic.com','t4.ftcdn.net', 'www.shutterstock.com'],
 
       },
-      output: "export",  // <=== enables static exports
-      reactStrictMode: true,
 };
 
 export default nextConfig;
